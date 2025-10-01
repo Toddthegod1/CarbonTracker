32166d82d6b87567b8ed2cce0d100869d0ba7a91
Toddthegod1 <toddjek@icloud.com>
2025-10-02 10:03:33 +1300

    Another indentation error

--- app.py/auth.py
+++ app.py/auth.py
@@ -15,8 +15,8 @@ DEMO_USER_ID = int(os.environ.get("DEMO_USER_ID", "1"))
 def require_user(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-    ensure_demo_user()
-    uid = request.headers.get("X-Demo-User")
+        ensure_demo_user()
+        uid = request.headers.get("X-Demo-User")
 try:
     uid = int(uid) if uid else DEMO_USER_ID
     except Exception:
