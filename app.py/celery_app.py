2484d3245227893a17c44fcf1741a96dc6ceeb6c
Toddthegod1 <toddjek@icloud.com>
2025-10-03 10:28:09 +1300

    Take 2

--- app.py/celery_app.py
+++ app.py/celery_app.py
@@ -1,10 +1,10 @@
+# /opt/carbon-tracker/app.py/celery_app.py
 from celery import Celery
 from kombu import Queue
 
 REGION = "us-east-1"
 ACCOUNT_ID = "330863234710"
 QUEUE = "carbon-jobs"
-CELERY_Q = "celery"  # broadcast/control queue
 
 celery = Celery(__name__, broker="sqs://")
 
@@ -15,11 +15,16 @@ celery.conf.update(
     broker_transport_options={
         "region": REGION,
         "predefined_queues": {
-            QUEUE:  {"url": f"https://sqs.{REGION}.amazonaws.com/{ACCOUNT_ID}/{QUEUE}"},
-            CELERY_Q: {"url": f"https://sqs.{REGION}.amazonaws.com/{ACCOUNT_ID}/{CELERY_Q}"},
+            QUEUE: {
+                "url": f"https://sqs.{REGION}.amazonaws.com/{ACCOUNT_ID}/{QUEUE}"
+            }
         },
         "visibility_timeout": 60,
         "polling_interval": 1,
     },
     worker_hijack_root_logger=False,
 )
+
+# >>> Disable Celery's remote-control/events so it doesn't use the 'celery' pidbox/broadcast queue
+celery.conf.worker_enable_remote_control = False
+celery.conf.worker_send_task_events = False
