0b62e62aa229c7486a6ec4813c2c34885dc37ede
Toddthegod1 <toddjek@icloud.com>
2025-10-03 10:10:27 +1300

    Celery only registers carbon-jobs

--- app.py/celery_app.py
+++ app.py/celery_app.py
@@ -1,13 +1,15 @@
 from celery import Celery
+from kombu import Queue
 
 REGION = "us-east-1"
 QUEUE = "carbon-jobs"
-ACCOUNT_ID = "330863234710"  
+ACCOUNT_ID = "330863234710"
 
 celery = Celery(__name__, broker="sqs://")
 
 celery.conf.update(
     task_default_queue=QUEUE,
+    task_queues=(Queue(QUEUE, routing_key=QUEUE),),
     task_default_routing_key=QUEUE,
     broker_transport_options={
         "region": REGION,
@@ -16,7 +18,7 @@ celery.conf.update(
                 "url": f"https://sqs.{REGION}.amazonaws.com/{ACCOUNT_ID}/{QUEUE}"
             }
         },
-        "visibility_timeout": 60,  # seconds
+        "visibility_timeout": 60,
         "polling_interval": 1,
     },
     worker_hijack_root_logger=False,
